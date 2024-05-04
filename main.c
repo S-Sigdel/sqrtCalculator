72153d2112d4eb90fe525cdf295a710899be158b
S-Sigdel <sakshyamsigdel@gmail.com>
2024-05-05 01:52:15 +0545

    precision checking advanced, vaiable name update

--- main.c
+++ main.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 int checkPrecision(int, float *, int);
 void getSqrt(int, float *);
+void getprecision(int *);
 void newLine() { printf("\n ========================================== \n"); }
 int main() {
   newLine();
-  printf("Enter a number you want to get squrare root of");
-  int a, precision;
-  float sqrt;
-  scanf("%d", &a);
+  printf("Enter a number you want to get squrare root of \t");
+  int numberToGetSquareRoot, precision;
+  scanf("%d", &numberToGetSquareRoot);
+  float sqrt = ((float)numberToGetSquareRoot) / 2;
+  newLine();
   printf("Enter the level of precision (decimals of precision) to calculate "
-         "the square root");
+         "the square root\n !!! Make sure that the precision is less than 7, "
+         "refer to ieee 754 for why!!!");
+  newLine();
   scanf("%d", &precision);
-  while (checkPrecision(a, &sqrt, precision)) {
-    getSqrt(a, &sqrt);
-  }
+  getSqrt(numberToGetSquareRoot, &sqrt);
+  newLine();
+  getprecision(&precision);
+  checkPrecision(numberToGetSquareRoot, &sqrt, precision);
+  printf("The square root of the number %d is %.2f", numberToGetSquareRoot,
+         sqrt);
 }
 
 int checkPrecision(int a, float *sqrt, int precision) {
   int numberToCompareWithPrecision = (int)(*sqrt) * precision * (*sqrt);
+  newLine();
+  printf("num to compare: %d", numberToCompareWithPrecision);
   int originalNumberWithPrecision = a * precision;
+  newLine();
+  printf("num to orginal Number: %d", originalNumberWithPrecision);
   return numberToCompareWithPrecision == originalNumberWithPrecision ? 0 : 1;
 }
-
-void getSqrt(int a, float *sqrt) { float half = ((float)a) / 2; }
+void getprecision(int *precision) {
+  int temp = *precision;
+  *precision = 1;
+  for (int i = 1; i <= temp; i++) {
+    (*precision) *= 10;
+  }
+  newLine();
+  printf("precision is: %d", *precision);
+}
+void getSqrt(int a, float *sqrt) {}
